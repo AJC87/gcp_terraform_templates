8a642faea49256fcde1d8e3a8abb7f87a4cb6231
Ashley Craig <ashleycraig19872@hotmail.co.uk>
2022-06-25 18:27:56 +0000

    Add subnet to VPC

--- tf/modules/vpc_network/main.tf
+++ tf/modules/vpc_network/main.tf
@@ -4,3 +4,13 @@ resource "google_compute_network" "vpc_network" {
 #   Thought this would be default
   auto_create_subnetworks = false
 }
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${var.name}-subnetwork"
+  description = var.description
+#   TODO: pass this in
+  ip_cidr_range = "10.0.1.0/25"
+#   TODO: pass this in
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network.id
+}
